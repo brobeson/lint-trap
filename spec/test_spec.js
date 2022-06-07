4a98ef7b5b49021ad3fd43e38bf77bb055c3607c
brobeson <brobeson@users.noreply.github.com>
2022-06-06 20:05:49 -0600

    add a failing spec

--- spec/test_spec.js
+++ spec/test_spec.js
@@ -2,4 +2,7 @@ describe("A suite", function() {
   it("contains spec with an expectation", function() {
     expect(true).toBe(true);
   });
+  it("contains spec with a failing expectation", function() {
+    expect(false).toBe(true);
+  });
 });
