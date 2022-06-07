f34a18494541af7c68868ac4479a70224a3652d5
brobeson <brobeson@users.noreply.github.com>
2022-06-06 21:03:28 -0600

    Update test_spec.js

--- spec/test_spec.js
+++ spec/test_spec.js
@@ -1,7 +1,10 @@
 const idx = require("../index.js")
 
 describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+  //it("contains spec with an expectation", function() {
+  //  expect(true).toBe(true);
+  //});
+  it("can load a results file", function() {
+    expect(idx.load_results("results.json")).not.toBe(null);
   });
 });
